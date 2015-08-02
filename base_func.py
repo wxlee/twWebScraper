34a7fb0c16a3295c8e6c40cc41a672dfc3d95817
Walker <walker1202@gmail.com>
2015-08-02 22:46:25 +0800

    update beautifulsoup

--- base_func.py
+++ base_func.py
@@ -1,12 +1,14 @@
 
 # coding: utf-8
 
-# In[8]:
+# In[11]:
 
 import requests
 
-url = ''
+from bs4 import BeautifulSoup
 
+#url = 'https://tw.search.buy.yahoo.com/search/shopping/product?p=windows+10&qt=product&cid=0&clv=0'
+url = 'https://tw.taobao.com/search/search.htm?_ksTS=1438526274940_101&spm=a213z.1224559.20141209.1&_input_charset=utf-8&navigator=all&json=on&q=%E5%8C%85%E5%8C%85&callback=__jsonp_cb&cna=XmXODdjnLBUCAQEio4ygSxSY&abtest=_AB-LR492-LR501-LR517-PR492-PR501-PR517'
 
 def web_request(url):
     # Set header
@@ -30,9 +32,12 @@ def web_request(url):
     
     return r
 
+r = web_request(url)
+r.encoding = r.apparent_encoding
+print r.text
 
-res = web_request(url)
+soup = BeautifulSoup(r.text, "html.parser")
 
-print res.content
-
+for item in soup.select('.info'):
+    print item.select('strong')
 
